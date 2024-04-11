48c6fd7559ae77e8b25d33e587cd1fa77edb572b
Satyam Kumar <satyam.kumar@vgroup.net>
2024-04-11 15:51:45 +0530

    resolved C2CAP-108 and C2CAP-110 issues

--- Example/C2CB-iOS-Library/ViewController.swift
+++ Example/C2CB-iOS-Library/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var mycustomeView:C2CSdkCustomView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let bundle = Bundle.main.bundleIdentifier
-        print("bundle___________",bundle ?? "")
         mycustomeView.channel_id = "657b0f55632f4ca91909ce0e"
        // self.DoneKeyboard(dismissOnTap: true)
     }
@@ -25,25 +23,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-}
-extension ViewController{
-    @IBAction func actionOnSmsBtn(_ sender:UIButton){
-        print("actionOnSmsBtn")
-//        SmsHandleView.onPresent(on: self){_ in
-//            print("actionOnSmsBtn 22")
-//        }
-    }
-    
-    @IBAction func actionOnCallBtn(_ sender:UIButton){
-        print("actionOnCallBtn")
-//        CallHandleView.onNaviagtionCallView(on: self){_ in
-//            print("actionOnCallBtn 11")
-//        }
-    }
-    @IBAction func actionOnEmailBtn(_ sender:UIButton){
-        print("actionOnEmailBtn")
-//        SmsValidationModel.onPresent(on: self){_ in
-//            print("actionOnCallBtn 11")
-//        }
-    }
 }
